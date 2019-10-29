f1c5ce6ea959872edeab4dc3fba8c545a1ec209a
MD.Zahangir Alom <tanveermdzahangir@gmail.com>
2019-10-29 09:33:27 +0600

    ji

---
+++ src/shoppingcart/Product.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shoppingcart;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Product {
+    int id;
+    String productName;
+    int quantity;
+    double price;
+
+    public Product() {
+    }
+
+    public Product(int id, String productName, int quantity, double price) {
+        this.id = id;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    
+    public ArrayList<Product> getProducts() {
+        ArrayList<Product> products = new ArrayList<Product>();
+        
+        // 
+        return products;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    
+}
